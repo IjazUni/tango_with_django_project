cb6af2ce37b4ceddc4f125c052b8c2fca2a8aea7
Ijaz Saleem <2773056s@student.gla.ac.uk>
2024-01-31 12:53:42 +0000

    Chapter 4

---
+++ rango/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    
+    context_dict={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    
+    context_dict={'boldmessage':'This tutorial has been put together by Ijaz'}
+
+    return render(request, 'rango/about.html', context=context_dict)
